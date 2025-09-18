488078ec430a54888bfb71969943bad1960262cc
Stefano1001 <santistebancoico@gmail.com>
2025-09-17 20:25:57 -0500

    Primer commit con React y Spring Boot

---
+++ Quizz-UTP-main/quizzutp/backend/src/test/java/com/example/quizzutp/QuizzutpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizzutp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzutpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
